f445d337adfe35d5baf4a52b804384cf54b93b1e
JAGUIAR.S <66434808+josedoce@users.noreply.github.com>
2023-11-09 03:02:32 -0300

    Update script.js

--- errometro/script.js
+++ errometro/script.js
@@ -81,26 +81,29 @@ body {
 
 document.body.insertAdjacentHTML('afterbegin', style);
 class Aviso {
-    constructor(title = "Mensagem x", text = "Texto da mensagem qualquer"){
+    constructor(title = "Mensagem x", text = "Texto da mensagem qualquer", onclose = function(){}){
         this.title = title;
         this.text = text;
+        this.onclose = onclose;
     }
     get content(){
         return {
             title: this.title,
-            message: this.text
+            message: this.text,
+            onclose: this.onclose
         }
     }
+
+    
 }
 
-function dialog({title, message}) {
+function dialog({title, message, onclose}) {
     const sizeIcon = 24;
     const icon = `<svg style="margin:0; width: ${sizeIcon}px; height:${sizeIcon};" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
     </svg>`
 
     const textSize = (text, base, ref = 0.02)=> {
-        
         text.split("").forEach((e)=>{
             base-= ref;
         })
@@ -128,6 +131,7 @@ function dialog({title, message}) {
     document.getElementById("errometro-style").insertAdjacentHTML('afterend', t);
     const modal = document.getElementById(`modal-${id}`);
     document.getElementById(`close-btn-${id}`).onclick = ()=> {
+        onclose();
         modal.remove();
     }
     id++;

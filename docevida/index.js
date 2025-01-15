2d0c28e7f830ada7b0ee5d6be10a89a89e729f41
JAGUIAR.S <66434808+josedoce@users.noreply.github.com>
2025-01-15 10:09:01 -0300

    Update index.js

--- docevida/index.js
+++ docevida/index.js
@@ -183,6 +183,10 @@ const produtos = [
 	{ id: 7252, tags: [ tags.ZEROACUCAR ]},
 	//parei aqui, onde o produto é filtrado por nome
 	{ id: 7254, tags: [ tags.ZEROACUCAR ]},
+	{ id: 7255, tags: [ tags.ZEROACUCAR ]},
+	{ id: 1665, tags: [ tags.ZEROACUCAR, tags.VEGANO, tags.SEMLACTOSE ]},
+	{ id: 2629, tags: [ tags.SEMLACTOSE ]},
+	{ id: 7332, tags: [ tags.SEMGLUTEN, tags.SEMLACTOSE ]}
 ];
 
 function tagnizar(produto) {
